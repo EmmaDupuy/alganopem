e8450da7f460be744b9763078f7e3996934b1a32
Emma Dupuy <emmadupuypro@gmail.com>
2021-09-15 16:28:40 +0200

    Try return to normal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,10 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatRadioModule} from '@angular/material/radio'; 
-import {MatSelectModule} from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
--- src/app/mock-data.ts
+++ src/app/mock-data.ts
@@ -2,27 +2,27 @@ import { Item } from './item';
 
 export const ITEMS: Item[] = [
     {
-        name:'10 minutes',
-        value:'10'
-     },
-     {
-         name:'20 minutes',
-         value:'20'
-      },
-      {
-          name:'30 minutes',
-          value:'30'
-       },
-       {
-           name:'40 minutes',
-           value:'40'
-        },
-        {
-            name:'50 minutes',
-            value:'50'
-        },
-        {
-            name:'60 minutes',
-            value:'60'
-        }
+        name: '10 minutes',
+        value: '10'
+    },
+    {
+        name: '20 minutes',
+        value: '20'
+    },
+    {
+        name: '30 minutes',
+        value: '30'
+    },
+    {
+        name: '40 minutes',
+        value: '40'
+    },
+    {
+        name: '50 minutes',
+        value: '50'
+    },
+    {
+        name: '60 minutes',
+        value: '60'
+    }
 ];
